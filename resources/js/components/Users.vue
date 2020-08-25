f77b16687d765afe3c7fc348c5a2ecd21dcf843c
SBTHDR <subrata.halder13d@gmail.com>
2020-08-25 22:20:21 +0600

    add profile routing

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="card-header bg-primary">Example Users</div>
+
+        <div class="card-body">
+            I'm an example Users.
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -184,17 +184,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <router-link to="/users" class="nav-link">
                   {{-- <i class="far fa-circle nav-icon"></i> --}}
-                  <i class="fas fa-circle-notch nav-icon indigo"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fas fa-circle-notch nav-icon indigo"></i>
-                  <p>Inactive Page</p>
-                </a>
+                  <i class="fas fa-users nav-icon indigo"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
             </ul>
           </li>
