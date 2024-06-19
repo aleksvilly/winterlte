e0a0c3810e046c78f2f5a61b499623817a823ab9
aleksvilly <49431859+aleksvilly@users.noreply.github.com>
2024-06-19 10:45:50 +0300

    first

--- public/js/app.js
+++ public/js/app.js
@@ -83203,8 +83203,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\winterlte\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\winterlte\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\winterlte\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\winterlte\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
